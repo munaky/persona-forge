7f21d01dd39f5d5c35579ce674c385e1570eb37d
munaky <010905mn@gmail.com>
2025-08-19 11:18:07 +0700

    feat(chatbot): add chatbot board, integrating chat api [2]

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,28 +1,38 @@
 import { NextResponse } from "next/server";
-import { GoogleGenAI } from "@google/genai";
+import { Chat, GoogleGenAI } from "@google/genai";
+import { ChatRequestPayload } from "@/types/chat";
+import { resSuccess } from "@/lib/response-format";
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GEMINI_API_KEY
 });
 
-export async function POST(req: Request){
-    const body = await req.json()
+export async function POST(req: Request) {
+    const body: ChatRequestPayload = await req.json()
 
     const chat = ai.chats.create({
-    model: "gemini-2.5-flash",
-    config: {
-        ...body.preset.config,
-        thinkingConfig: {
-            thinkingBudget: body.preset.thinking ? -1 : 0,
+        model: "gemini-2.5-flash",
+        config: {
+            ...body.preset.config,
+            thinkingConfig: {
+                thinkingBudget: body.preset.thinking ? -1 : 0,
+            },
+            responseModalities: ['TEXT']
         },
-        responseModalities: 'TEXT'
-    },
-    history: body.preset.remember ? body.history : []
-  });
+        history: body.preset.remember ? body.history : []
+    });
 
-  const res = await chat.sendMessage({
-        message: body.message,
+    const res = await chat.sendMessage({
+        message: body.userInput,
     })
 
-    return NextResponse.json(res);
+    return NextResponse.json(resSuccess({
+        data: {
+            id: crypto.randomUUID(),
+            role: "model",
+            parts: [
+                { text: res.text || "No response from AI" }
+            ],
+        }
+    }));
 }
