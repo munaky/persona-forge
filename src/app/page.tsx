1a87bab5d6896370b8453a57f2cbbd3888d92d4c
munaky <010905mn@gmail.com>
2025-08-20 10:59:13 +0700

    feat(chatbot): store chat state to local storage  and add FullScreenLoging component [9]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import ChatBoard from "@/components/ChatBoard";
+import FullScreenLoader from "@/components/FullScreenLoading";
 import PresetCard from "@/components/PresetCard";
 import { ChatState } from "@/types/chat";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Main() {
-  const [chatState, setChatState] = useState<ChatState | null>({
+const defaultChatState: ChatState = {
   preset: {
-    id: "default",
+    id: "new",
     name: "Default",
     description: "Default preset for chat",
     thinking: false,
@@ -18,11 +18,33 @@ export default function Main() {
     },
   },
   history: [],
-});
+}
+
+
+export default function Main() {
+  const [chatState, setChatState] = useState<ChatState | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const chatStateFromStorage = localStorage.getItem("chatState");
+    if (chatStateFromStorage) {
+      setChatState(JSON.parse(chatStateFromStorage));
+    } else {
+      setChatState(defaultChatState);
+    }
+    setLoading(false);
+  }, [])
+
+  useEffect(() => {
+    if (chatState) {
+      localStorage.setItem("chatState", JSON.stringify(chatState));
+    }
+  }, [chatState]);
 
   return (
     <>
-    <div className="flex ">
+    <div className="flex overflow-hidden h-screen">
+      <FullScreenLoader loading={loading} />
       <ChatBoard chatState={chatState} setChatState={setChatState} />
       <PresetCard chatState={chatState} setChatState={setChatState} />
     </div>
